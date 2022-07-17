82f640a742c9c1b089e710a9de180a869a1aee58
logicwang <1758555877@qq.com>
2022-07-17 23:55:49 +0800

    aaa

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies: true
-})
-
-publicPath: process.env.NODE_ENV === 'production' ?
-    '/loigcwang.github.io/' //这里写展示页面的仓库名称
-    :
-    '/'
